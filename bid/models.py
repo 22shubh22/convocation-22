e45297776f7c79ecae1c2dfb6171237d9092606a
Shubham Goyal <22shubh22@gmail.com>
2022-07-23 21:33:14 +0530

    fix: data entry validation

--- bid/models.py
+++ bid/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Bid(models.Model):
-    bidder_name = models.CharField(max_length=200, null=True, blank=True)
-    bid_amount = models.IntegerField(null=True, blank=True)
+    bidder_name = models.CharField(max_length=200)
+    bid_amount = models.IntegerField()
     choices = (("1", "Shreya Ananad"), ("2", "Shubham Upadhyay"), ("3", "Raashid Altaf"))
-    vote = models.CharField(choices=choices, max_length=2, null=True, blank=True)
+    vote = models.CharField(choices=choices, max_length=2)
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
